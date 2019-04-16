780611a589284f9873606308c7ec0ed75fca697a
Tim Hall <tim.hall.engr@gmail.com>
2019-04-16 10:04:17 -0400

    Implement writer insert

--- src/writer.ts
+++ src/writer.ts
@@ -13,9 +13,13 @@ import {
   isInlineTableItem,
   isInlineArrayItem,
   Key,
-  Value
+  Value,
+  InlineArray,
+  InlineArrayItem,
+  InlineTableItem
 } from './ast';
 import { Span, getSpan, clonePosition } from './location';
+import { last } from './utils';
 
 type Offsets = WeakMap<Node, Span>;
 const staged_offsets: WeakMap<AST, Offsets> = new WeakMap();
@@ -71,7 +75,7 @@ export function replace(ast: AST, parent: Node, existing: Node, replacement: Nod
 
   replacement.loc.start = clonePosition(existing.loc.start);
   replacement.loc.end = {
-    line: replacement.loc.start.line + replacement_span.lines,
+    line: replacement.loc.start.line + replacement_span.lines - 1,
     column: replacement.loc.start.column + replacement_span.columns
   };
 
@@ -82,17 +86,57 @@ export function replace(ast: AST, parent: Node, existing: Node, replacement: Nod
   offsets.set(replacement, { lines, columns });
 }
 
-export function insert(ast: AST, parent: Node, child: Node, index?: number | 'key' | 'value') {
-  const offsets = getStaged(ast);
-  if (hasItems(parent)) {
-    //
-  } else if (hasItem(parent)) {
-    //
-  } else if (isKeyValue(parent)) {
-    //
-  } else {
+export function insert(ast: AST, parent: Node, child: Node, index?: number) {
+  if (!hasItems(parent)) {
     throw new Error(`Unsupported parent type "${parent.type}" for replace.`);
   }
+
+  const previous = index != null ? parent.items[index - 1] : last(parent.items);
+
+  // Add commas as-needed
+  const is_inline = isInlineArray(parent) || isInlineTable(parent);
+  if (previous && is_inline) {
+    (previous as InlineArrayItem | InlineTableItem).comma = true;
+  }
+  if (index != null && parent.items.length >= index + 1 && is_inline) {
+    (child as InlineArrayItem | InlineTableItem).comma = true;
+  }
+
+  // Set start location from previous item or start of array
+  // (previous is undefined for empty array or inserting at first item)
+  const child_span = getSpan(child.loc);
+  const use_new_line =
+    isTable(parent) || isTableArray(parent) || (isInlineArray(parent) && perLine(parent));
+
+  child.loc.start = previous
+    ? {
+        line: previous.loc.start.line,
+        column: use_new_line ? previous.loc.start.column : previous.loc.end.column
+      }
+    : clonePosition(parent.loc.start);
+
+  if (use_new_line) {
+    child.loc.start.line += 1;
+  } else {
+    child.loc.start.column += previous ? 2 : 1;
+  }
+
+  child.loc.end = {
+    line: child.loc.start.line + child_span.lines - 1,
+    column: child.loc.start.column + child_span.columns
+  };
+
+  if (index != null) {
+    parent.items.splice(index, 0, child);
+  } else {
+    parent.items.push(child);
+  }
+
+  const offsets = getStaged(ast);
+  offsets.set(child, {
+    lines: child_span.lines - (use_new_line ? 0 : 1),
+    columns: child_span.columns
+  });
 }
 
 export function remove(ast: AST, parent: Node, node: Node) {
@@ -108,9 +152,16 @@ export function remove(ast: AST, parent: Node, node: Node) {
   }
 }
 
-export function write(ast: AST) {
+export function applyWrites(ast: AST) {
   const offsets = getStaged(ast);
   staged_offsets.delete(ast);
 
   //
+}
+
+function perLine(array: InlineArray): boolean {
+  if (!array.items.length) return false;
+
+  const span = getSpan(array.loc);
+  return span.lines > array.items.length;
 }
