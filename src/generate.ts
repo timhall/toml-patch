f71f8236bf3ce17c76444c5a2fc9c1bf52b9616c
Tim Hall <tim.hall.engr@gmail.com>
2019-04-23 10:52:38 -0400

    Add helpers to generate nodes

---
+++ src/generate.ts
@@ -0,0 +1,221 @@
+import {
+  NodeType,
+  Document,
+  Table,
+  TableKey,
+  TableArray,
+  TableArrayKey,
+  Value,
+  KeyValue,
+  Key,
+  String,
+  Integer,
+  Float,
+  Boolean,
+  DateTime,
+  InlineArray,
+  InlineItem,
+  InlineTable,
+  Comment
+} from './ast';
+import { zero, cloneLocation, clonePosition } from './location';
+import { shiftNode } from './writer';
+
+export function generateDocument(): Document {
+  return {
+    type: NodeType.Document,
+    loc: { start: zero(), end: zero() },
+    items: []
+  };
+}
+
+export function generateTable(key: string[]): Table {
+  const table_key = generateTableKey(key);
+
+  return {
+    type: NodeType.Table,
+    loc: cloneLocation(table_key.loc),
+    key: table_key,
+    items: []
+  };
+}
+
+export function generateTableKey(key: string[]): TableKey {
+  const raw = keyValueToRaw(key);
+
+  return {
+    type: NodeType.TableKey,
+    loc: {
+      start: zero(),
+      end: { line: 1, column: raw.length + 2 }
+    },
+    item: {
+      type: NodeType.Key,
+      loc: {
+        start: { line: 1, column: 1 },
+        end: { line: 1, column: raw.length + 1 }
+      },
+      value: key,
+      raw
+    }
+  };
+}
+
+export function generateTableArray(key: string[]): TableArray {
+  const table_array_key = generateTableArrayKey(key);
+
+  return {
+    type: NodeType.TableArray,
+    loc: cloneLocation(table_array_key.loc),
+    key: table_array_key,
+    items: []
+  };
+}
+
+export function generateTableArrayKey(key: string[]): TableArrayKey {
+  const raw = keyValueToRaw(key);
+
+  return {
+    type: NodeType.TableArrayKey,
+    loc: {
+      start: zero(),
+      end: { line: 1, column: raw.length + 4 }
+    },
+    item: {
+      type: NodeType.Key,
+      loc: {
+        start: { line: 1, column: 2 },
+        end: { line: 1, column: raw.length + 2 }
+      },
+      value: key,
+      raw
+    }
+  };
+}
+
+export function generateKeyValue(key: string[], value: Value): KeyValue {
+  const key_node = generateKey(key);
+  const { column } = key_node.loc.end;
+
+  const equals = column + 1;
+
+  shiftNode(
+    value,
+    { lines: 0, columns: column + 3 - value.loc.start.column },
+    { first_line_only: true }
+  );
+
+  return {
+    type: NodeType.KeyValue,
+    loc: {
+      start: clonePosition(key_node.loc.start),
+      end: clonePosition(value.loc.end)
+    },
+    key: key_node,
+    equals,
+    value
+  };
+}
+
+const IS_BARE_KEY = /[\w,\d,\_,\-]+/;
+function keyValueToRaw(value: string[]): string {
+  return value.map(part => (IS_BARE_KEY.test(part) ? part : JSON.stringify(part))).join('.');
+}
+
+export function generateKey(value: string[]): Key {
+  const raw = keyValueToRaw(value);
+
+  return {
+    type: NodeType.Key,
+    loc: { start: zero(), end: { line: 1, column: raw.length } },
+    raw,
+    value
+  };
+}
+
+export function generateString(value: string): String {
+  const raw = JSON.stringify(value);
+
+  return {
+    type: NodeType.String,
+    loc: { start: zero(), end: { line: 1, column: raw.length } },
+    raw,
+    value
+  };
+}
+
+export function generateInteger(value: number): Integer {
+  const raw = String(value);
+
+  return {
+    type: NodeType.Integer,
+    loc: { start: zero(), end: { line: 1, column: raw.length } },
+    raw,
+    value
+  };
+}
+
+export function generateFloat(value: number): Float {
+  const raw = String(value);
+
+  return {
+    type: NodeType.Float,
+    loc: { start: zero(), end: { line: 1, column: raw.length } },
+    raw,
+    value
+  };
+}
+
+export function generateBoolean(value: boolean): Boolean {
+  return {
+    type: NodeType.Boolean,
+    loc: { start: zero(), end: { line: 1, column: value ? 4 : 5 } },
+    value
+  };
+}
+
+export function generateDateTime(value: Date): DateTime {
+  const raw = value.toISOString();
+
+  return {
+    type: NodeType.DateTime,
+    loc: { start: zero(), end: { line: 1, column: raw.length } },
+    raw,
+    value
+  };
+}
+
+export function generateInlineArray(): InlineArray {
+  return {
+    type: NodeType.InlineArray,
+    loc: { start: zero(), end: { line: 1, column: 2 } },
+    items: []
+  };
+}
+
+export function generateInlineItem(item: KeyValue | Value, comma: boolean = true): InlineItem {
+  return {
+    type: NodeType.InlineItem,
+    loc: cloneLocation(item.loc),
+    item,
+    comma
+  };
+}
+
+export function generateInlineTable(): InlineTable {
+  return {
+    type: NodeType.InlineTable,
+    loc: { start: zero(), end: { line: 1, column: 2 } },
+    items: []
+  };
+}
+
+export function generateComment(comment: string): Comment {
+  if (!comment.startsWith('#')) comment = `# ${comment}`;
+
+  return {
+    type: NodeType.Comment,
+    loc: { start: zero(), end: { line: 1, column: comment.length } },
+    raw: comment
+  };
+}
